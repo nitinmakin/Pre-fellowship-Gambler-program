0749c4b5df083555eaf186dab57530378a12f7c3
unknown <nitinmakin124.com>
2020-08-21 23:07:27 +0530

    Added- resign of the day after win or loss 50%

--- GambleSimulation.java
+++ GambleSimulation.java
@@ -1,9 +1,51 @@
+import java.util.Random;
 
 public class GambleSimulation 
 {
-  public static void WelcomeMessage()
-  {
-	  System.out.println("gambler is starting bet from $100 and putting $1 in every bet");
-          System.out.println("he win or loss $1 at a time");
-  }
+	static int initialAmount = 100;
+	static int betPlaced = 1;
+	static int stopOfDayLoss=50;
+	static int stopOfDayWin=150;
+
+	public static void WelcomeMessage()
+
+	{
+		System.out.println("Gambler is starting bet from $100");
+		System.out.println("He putting $1 in every bet so he win or loss $1 at a time");
+		System.out.println("------------------------------");
+	}
+
+	public static int generateRandomNo()
+	{
+		Random random=new Random();
+		return random.nextInt(2);
+	}
+
+	public static void resignOfTheDay()
+	{
+
+		while(initialAmount > 50 && initialAmount < 150)
+		{
+			int random=generateRandomNo();
+
+			if(random == 0)
+				initialAmount--;
+
+			else
+				initialAmount++;
+		}
+		if(initialAmount == stopOfDayLoss)
+			System.out.println("He LOST for a day----> $"+initialAmount);
+
+		else
+			System.out.println("He WON for the day----> $"+(initialAmount-100));
+	}
+
+	public static void main(String[] args)
+	{
+		WelcomeMessage();
+		resignOfTheDay();
+
+	}
 }
+

843b077d12f29138a242d3eee7826d89d800ccdc
unknown <nitinmakin124.com>
2020-08-21 22:37:50 +0530

    Added- win or loss  at a time

--- GambleSimulation.java
+++ GambleSimulation.java
@@ -4,5 +4,6 @@ public class GambleSimulation
   public static void WelcomeMessage()
   {
 	  System.out.println("gambler is starting bet from $100 and putting $1 in every bet");
+          System.out.println("he win or loss $1 at a time");
   }
 }
